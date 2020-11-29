b8636bfae7002eb1c0b7b72281dcc9761b465993
qmohitsingh <qmohitsingh@gmail.com>
2020-11-29 13:23:06 +0530

    finished configuration of webpack

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+
+ return (
+	<div>
+		Webpack Zero done! Yay
+	</div>
+)
+
+}
+
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    target: "web",
+    entry: "./src/index",
+    output: {
+        path: path.resolve(__dirname, "build"),
+        publicPath: "/",
+        filename: "bundle.js"
+    },
+    devServer: {
+        stats: "minimal",
+        overlay: true,
+        historyApiFallback: true,
+        disableHostCheck: true,
+        headers: { "Access-Control-Allow-Origin": "*" },
+        https: false
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /(\.css)$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    }
+};
+
